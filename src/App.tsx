fe62fd0afdde48fd2f134b7d62ff670cfa8a92c8
islomnumanov <islom4ik.numanov@gmail.com>
2021-12-01 13:18:50 +0900

    breakpoint - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
 import './App.css';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
+  const PersonName = {
+    first: 'David',
+    last: 'Letterman'
+  }
+
+  const PersonListArray = [
+    {
+      first: 'Alan',
+      last: 'Walker'
+    },
+    {
+      first: 'Skyle',
+      last: 'Gray'
+    },
+    {
+      first: 'Jessica',
+      last: 'Hardman'
+    }
+  ]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Greet name='Jessie' messageCount={20} isLoggedIn={true}/>
+        <Person name={PersonName}/>
+        <PersonList names={PersonListArray}/> 
+        <Status status='success'/>
+        <Heading>This is a child prop</Heading>
+        <Oscar>
+          <Heading>Oscar goes to Leonardo Dicaprio</Heading>
+        </Oscar>
+        <Greet name='Mike' isLoggedIn={true} />
+        <Button handleClick={(event, id) => console.log('Button has been clicked', event, id)}/>
+        <Input value='' handleChange={event => console.log(event)} />
+        <Container styles={{ border: '1px solid black', padding: '1rem' }} />
     </div>
   );
 }
---
+++ src/components/Greet.tsx
@@ -0,0 +1,19 @@
+type GreetProps = {
+    name: string,
+    messageCount?: number,
+    isLoggedIn: boolean
+}
+
+export const Greet = (props: GreetProps) => {
+    const { messageCount = 0 } = props
+    return (
+        <div>
+            <h2>
+            {
+                    props.isLoggedIn ? `Welcome ${props.name} you have ${messageCount} unread messages from gmail` : 'Welcome Guest'
+                }
+                </h2>
+            
+        </div>
+    )
+}
